0966f0cd7e8cb7b4a8aeebc6fc233e30447cc10b
Paul Vogel <github@paulvogel.me>
2019-03-18 17:16:05 +0100

    Tests: Improved coverage for money commands

--- src/test/java/com/greatmancode/craftconomy3/commands/TestMoneyCommands.java
+++ src/test/java/com/greatmancode/craftconomy3/commands/TestMoneyCommands.java
@@ -1,20 +1,20 @@
 /**
  * This file is part of Craftconomy3.
- *
+ * <p>
  * Copyright (c) 2011-2016, Greatman <http://github.com/greatman/>
  * Copyright (c) 2016-2017, Aztorius <http://github.com/Aztorius/>
  * Copyright (c) 2018, Pavog <http://github.com/pavog/>
- *
+ * <p>
  * Craftconomy3 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Craftconomy3 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with Craftconomy3.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,10 +23,7 @@ package com.greatmancode.craftconomy3.commands;
 import com.greatmancode.craftconomy3.Common;
 import com.greatmancode.craftconomy3.TestCommandSender;
 import com.greatmancode.craftconomy3.TestInitializator;
-import com.greatmancode.craftconomy3.commands.money.BalanceCommand;
-import com.greatmancode.craftconomy3.commands.money.CreateCommand;
-import com.greatmancode.craftconomy3.commands.money.DeleteCommand;
-import com.greatmancode.craftconomy3.commands.money.GiveCommand;
+import com.greatmancode.craftconomy3.commands.money.*;
 import com.greatmancode.tools.commands.PlayerCommandSender;
 import org.junit.After;
 import org.junit.Before;
@@ -61,7 +58,7 @@ public class TestMoneyCommands {
         Common.getInstance().getAccountManager().getAccount(TEST_USER.getName(), false);
         BalanceCommand command = new BalanceCommand(null);
         // User exists
-        command.execute(TEST_USER,new String[]{TEST_USER.getName()});
+        command.execute(TEST_USER, new String[]{TEST_USER.getName()});
         // User does not exist
         command.execute(TEST_USER, new String[]{"unknown"});
         // No user given
@@ -71,7 +68,7 @@ public class TestMoneyCommands {
     @Test
     public void testCreateCommand() {
         CreateCommand command = new CreateCommand(null);
-        command.execute(TEST_USER, new String[] {"testaccount"});
+        command.execute(TEST_USER, new String[]{"testaccount"});
         assertTrue(Common.getInstance().getAccountManager().exist("testaccount", false));
         command.execute(TEST_USER, new String[]{"testaccount"});
     }
@@ -81,11 +78,11 @@ public class TestMoneyCommands {
         Common.getInstance().getAccountManager().getAccount("testaccount", false);
         DeleteCommand command = new DeleteCommand(null);
         // Account exists
-        command.execute(TEST_USER, new String[] {"testaccount"});
+        command.execute(TEST_USER, new String[]{"testaccount"});
         // Account should no longer exist
         assertFalse(Common.getInstance().getAccountManager().exist("testaccount", false));
         // Check again with an account that never existed
-        command.execute(TEST_USER, new String[] {"testaccountwithaverylongname"});
+        command.execute(TEST_USER, new String[]{"testaccountwithaverylongname"});
     }
 
     @Test
@@ -113,9 +110,45 @@ public class TestMoneyCommands {
         command.execute(TEST_USER, new String[]{TEST_USER.getName(), "200", defaultCurrencyName, "MyCustomWorldWithALongName"});
         assertEquals(initialValue + 600, Common.getInstance().getAccountManager().getAccount(TEST_USER.getName(), false).getBalance("UnitTestWorld", defaultCurrencyName), 0);
     }
-    
-    private PlayerCommandSender createTestUser(String name){
+
+    @Test
+    public void testInfiniteCommand() {
+        // TODO Test if this worked (with accountmanager)
+
+        InfiniteCommand command = new InfiniteCommand("infinite");
+
+        // Test with unknown user
+        command.execute(TEST_USER, new String[]{"unknown"});
+
+        // Turn on for user
+        command.execute(TEST_USER, new String[]{TEST_USER.getName()});
+        // Turn off for user
+        command.execute(TEST_USER, new String[]{TEST_USER.getName()});
+    }
+
+    @Test
+    public void testSetCommand() {
+        // TODO Test if this worked (with accountmanager)
+
+        SetCommand command = new SetCommand("set");
+        String defaultCurrencyName = Common.getInstance().getCurrencyManager().getDefaultCurrency().getName();
+
+        // Test without amount
+        command.execute(TEST_USER, new String[]{TEST_USER.getName()});
+
+        // Test with own user
+        command.execute(TEST_USER, new String[]{TEST_USER.getName(), "100"});
+        // Test with own user and default currency
+        command.execute(TEST_USER, new String[]{TEST_USER.getName(), "100", defaultCurrencyName});
+
+        // Test with other user
+        command.execute(TEST_USER, new String[]{TEST_USER2.getName(), "100"});
+        // Test with other user and default currency
+        command.execute(TEST_USER, new String[]{TEST_USER2.getName(), "100", defaultCurrencyName});
+    }
+
+    private PlayerCommandSender createTestUser(String name) {
         UUID uuid = UUID.randomUUID();
-        return new PlayerCommandSender<>(name,uuid,new TestCommandSender(uuid,name));
+        return new PlayerCommandSender<>(name, uuid, new TestCommandSender(uuid, name));
     }
 }
