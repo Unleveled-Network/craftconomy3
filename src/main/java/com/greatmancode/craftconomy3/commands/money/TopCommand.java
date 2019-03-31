d4c55a347631d0706876655646cc0af3ae22c111
Paul Vogel <github@paulvogel.me>
2019-03-31 21:27:09 +0200

    TopCommand: Use players world to determine world group name
    If the player is not in a world we use the default world group name.

--- src/main/java/com/greatmancode/craftconomy3/commands/money/TopCommand.java
+++ src/main/java/com/greatmancode/craftconomy3/commands/money/TopCommand.java
@@ -1,26 +1,27 @@
 /**
  * This file is part of Craftconomy3.
- *
+ * <p>
  * Copyright (c) 2011-2016, Greatman <http://github.com/greatman/>
  * Copyright (c) 2016-2017, Aztorius <http://github.com/Aztorius/>
  * Copyright (c) 2018, Pavog <http://github.com/pavog/>
- *
+ * <p>
  * Craftconomy3 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Craftconomy3 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with Craftconomy3.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.greatmancode.craftconomy3.commands.money;
 
 import com.greatmancode.craftconomy3.Common;
+import com.greatmancode.craftconomy3.account.Account;
 import com.greatmancode.craftconomy3.commands.AbstractCommand;
 import com.greatmancode.craftconomy3.currency.Currency;
 import com.greatmancode.craftconomy3.groups.WorldGroupsManager;
@@ -42,26 +43,26 @@ class TopCommandThread implements Runnable {
 
         @Override
         public void run() {
-            Common.getInstance().getServerCaller().getPlayerCaller().sendMessage(sender, ret,"top");
+            Common.getInstance().getServerCaller().getPlayerCaller().sendMessage(sender, ret, "top");
         }
     }
 
     private final CommandSender sender;
     private final int page;
-    private final String world;
+    private final String worldGroup;
     private final Currency currency;
 
-    public TopCommandThread(CommandSender sender, int page, String world, Currency currency) {
+    public TopCommandThread(CommandSender sender, int page, String worldGroup, Currency currency) {
         this.sender = sender;
         this.page = page;
-        this.world = world;
+        this.worldGroup = worldGroup;
         this.currency = currency;
     }
 
     @Override
     public void run() {
-        String ret = Common.getInstance().getLanguageManager().parse("money_top_header", page, world) + "\n";
-        List<TopCommand.TopEntry> list = Common.getInstance().getStorageHandler().getStorageEngine().getTopEntry(page, currency, world);
+        String ret = Common.getInstance().getLanguageManager().parse("money_top_header", page, worldGroup) + "\n";
+        List<TopCommand.TopEntry> list = Common.getInstance().getStorageHandler().getStorageEngine().getTopEntry(page, currency, worldGroup);
         for (int i = 0; i < list.size(); i++) {
             TopCommand.TopEntry entry = list.get(i);
             ret += "" + ((page - 1) * NUMBER_ELEMENTS + i + 1) + ": {{DARK_GREEN}}" + entry.username + " {{WHITE}}" + Common.getInstance().format(null, currency, entry.balance) + "\n";
@@ -78,11 +79,9 @@ public class TopCommand extends AbstractCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        int page = 1;
-        Currency currency;
-        if (args.length == 0) {
-            currency = Common.getInstance().getCurrencyManager().getDefaultCurrency();
-        } else {
+        // Check if we have a currency name or use the default currency
+        Currency currency = Common.getInstance().getCurrencyManager().getDefaultCurrency();
+        if (args.length > 0) {
             if (Common.getInstance().getCurrencyManager().getCurrency(args[0]) != null) {
                 currency = Common.getInstance().getCurrencyManager().getCurrency(args[0]);
             } else {
@@ -91,6 +90,8 @@ public class TopCommand extends AbstractCommand {
             }
         }
 
+        // Check if we have a page or use page 1
+        int page = 1;
         if (args.length > 1) {
             try {
                 page = Integer.parseInt(args[1]);
@@ -104,12 +105,22 @@ public class TopCommand extends AbstractCommand {
             }
         }
 
-        String world = WorldGroupsManager.DEFAULT_GROUP_NAME;
-        if (args.length > 2) {
-            world = args[2];
+        // Check if we have a world or use the world the player is currently in
+        String worldGroupName = Account.getWorldGroupOfPlayerCurrentlyIn(sender.getUuid());
+        // If the player is not in a worldgroup we use the default world group name
+        if (worldGroupName == null) {
+            worldGroupName = WorldGroupsManager.DEFAULT_GROUP_NAME;
         }
 
-        Common.getInstance().getServerCaller().getSchedulerCaller().delay(new TopCommandThread(sender, page, world, currency), 0, false);
+        if (args.length > 2) {
+            if (!Common.getInstance().getServerCaller().worldExist(args[2])) {
+                sendMessage(sender, Common.getInstance().getLanguageManager().getString("world_not_exist"));
+                return;
+            }
+            worldGroupName = Common.getInstance().getWorldGroupManager().getWorldGroupName(args[2]);
+        }
+
+        Common.getInstance().getServerCaller().getSchedulerCaller().delay(new TopCommandThread(sender, page, worldGroupName, currency), 0, false);
     }
 
     @Override
