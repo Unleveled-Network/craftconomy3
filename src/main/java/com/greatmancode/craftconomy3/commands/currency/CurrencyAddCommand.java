bad0c71e02286f23fb3cf93403753bcc0506a5e2
Paul Vogel <github@paulvogel.me>
2019-04-08 19:28:12 +0200

    Currency add command: Check for correct number of arguments when executing
    To not break tests

--- src/main/java/com/greatmancode/craftconomy3/commands/currency/CurrencyAddCommand.java
+++ src/main/java/com/greatmancode/craftconomy3/commands/currency/CurrencyAddCommand.java
@@ -1,20 +1,20 @@
 /**
  * This file is part of Craftconomy3.
- *
+ * <p>
  * Copyright (c) 2011-2016, Greatman <http://github.com/greatman/>
  * Copyright (c) 2016-2017, Aztorius <http://github.com/Aztorius/>
  * Copyright (c) 2018, Pavog <http://github.com/pavog/>
- *
+ * <p>
  * Craftconomy3 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Craftconomy3 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with Craftconomy3.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,13 +25,14 @@ import com.greatmancode.craftconomy3.commands.AbstractCommand;
 import com.greatmancode.tools.commands.CommandSender;
 
 public class CurrencyAddCommand extends AbstractCommand {
+
     public CurrencyAddCommand(String name) {
         super(name);
     }
-    
+
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (args[0] != null && args[1] != null && args[2] != null && args[3] != null && args[4] != null) {
+        if (args.length > 4 && args[0].length() > 0 && args[1].length() > 0 && args[2].length() > 0 && args[3].length() > 0 && args[4].length() > 0) {
             if (Common.getInstance().getCurrencyManager().getCurrency(args[0]) == null) {
                 Common.getInstance().getCurrencyManager().addCurrency(args[0], args[1], args[2], args[3], args[4], true);
                 sendMessage(sender, Common.getInstance().getLanguageManager().getString("currency_added"));
