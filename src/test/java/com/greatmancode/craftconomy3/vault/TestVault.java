5d7464479401b8dd788c507c2b0a5de312aa3f64
Paul Vogel <github@paulvogel.me>
2019-05-13 20:00:15 +0200

    Tests: Improve test coverage on vault integration

--- src/test/java/com/greatmancode/craftconomy3/vault/TestVault.java
+++ src/test/java/com/greatmancode/craftconomy3/vault/TestVault.java
@@ -111,6 +111,11 @@ public class TestVault {
 
         assertEquals(100.0D, this.economy.getBalance(mockedOfflinePlayer), 0.0D);
         assertEquals(100.0D, this.economy.getBalance(mockedOfflinePlayer, "UnitTestWorld"), 0.0D);
+
+        // Test default value (0.0) without creating an account
+        MockedOfflinePlayer mockedOfflinePlayer2 = new MockedOfflinePlayer(TEST_USER2.getUuid(), TEST_USER2.getName());
+        assertEquals(0.0D, this.economy.getBalance(mockedOfflinePlayer2), 0.0D);
+        assertEquals(0.0D, this.economy.getBalance(mockedOfflinePlayer2, "UnitTestWorld"), 0.0D);
     }
 
     @Test
@@ -126,16 +131,69 @@ public class TestVault {
         assertTrue(this.economy.has(mockedOfflinePlayer, "UnitTestWorld", 50.0D));
         assertFalse(this.economy.has(mockedOfflinePlayer, 500.0D));
         assertFalse(this.economy.has(mockedOfflinePlayer, "UnitTestWorld", 500.0D));
+
+        // Test default (false) without creating an account
+        MockedOfflinePlayer mockedOfflinePlayer2 = new MockedOfflinePlayer(TEST_USER2.getUuid(), TEST_USER2.getName());
+        assertFalse(this.economy.has(mockedOfflinePlayer2, 50.0D));
+        assertFalse(this.economy.has(mockedOfflinePlayer2, "UnitTestWorld", 50.0D));
     }
 
     @Test
     public void testWithdrawPlayer() {
-        // TODO
+        final String defaultCurrencyName = Common.getInstance().getCurrencyManager().getDefaultCurrency().getName();
+        MockedOfflinePlayer mockedOfflinePlayer = new MockedOfflinePlayer(TEST_USER.getUuid(), TEST_USER.getName());
+        MockedOfflinePlayer mockedOfflinePlayer2 = new MockedOfflinePlayer(TEST_USER2.getUuid(), TEST_USER2.getName());
+
+        // Create test account and set its balance
+        Account account = Common.getInstance().getAccountManager().getAccount(TEST_USER.getName(), false);
+        account.set(100.0D, "UnitTestWorld", defaultCurrencyName, Cause.UNKNOWN, "Unittest");
+
+        EconomyResponse response1 = this.economy.withdrawPlayer(mockedOfflinePlayer, 10.0D);
+        assertEquals(EconomyResponse.ResponseType.SUCCESS, response1.type);
+        assertEquals(90.0D, response1.balance, 0.0D);
+
+        EconomyResponse response2 = this.economy.withdrawPlayer(mockedOfflinePlayer, "UnitTestWorld", 10.0D);
+        assertEquals(EconomyResponse.ResponseType.SUCCESS, response2.type);
+        assertEquals(80.0D, response2.balance, 0.0D);
+
+
+        // Test default (Error response) without creating an account
+        EconomyResponse errorResponse1 = this.economy.withdrawPlayer(mockedOfflinePlayer2, 10.0D);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse1.type);
+        assertEquals(0.0D, errorResponse1.balance, 0.0D);
+
+        EconomyResponse errorResponse2 = this.economy.withdrawPlayer(mockedOfflinePlayer2, "UnitTestWorld", 10.0D);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse2.type);
+        assertEquals(0.0D, errorResponse2.balance, 0.0D);
     }
 
     @Test
     public void testDepositPlayer() {
-        // TODO
+        final String defaultCurrencyName = Common.getInstance().getCurrencyManager().getDefaultCurrency().getName();
+        MockedOfflinePlayer mockedOfflinePlayer = new MockedOfflinePlayer(TEST_USER.getUuid(), TEST_USER.getName());
+        MockedOfflinePlayer mockedOfflinePlayer2 = new MockedOfflinePlayer(TEST_USER2.getUuid(), TEST_USER2.getName());
+
+        // Create test account and set its balance
+        Account account = Common.getInstance().getAccountManager().getAccount(TEST_USER.getName(), false);
+        account.set(100.0D, "UnitTestWorld", defaultCurrencyName, Cause.UNKNOWN, "Unittest");
+
+        EconomyResponse response1 = this.economy.depositPlayer(mockedOfflinePlayer, 10.0D);
+        assertEquals(EconomyResponse.ResponseType.SUCCESS, response1.type);
+        assertEquals(110.0D, response1.balance, 0.0D);
+
+        EconomyResponse response2 = this.economy.depositPlayer(mockedOfflinePlayer, "UnitTestWorld", 10.0D);
+        assertEquals(EconomyResponse.ResponseType.SUCCESS, response2.type);
+        assertEquals(120.0D, response2.balance, 0.0D);
+
+
+        // Test default (Error response) without creating an account
+        EconomyResponse errorResponse1 = this.economy.depositPlayer(mockedOfflinePlayer2, 10.0D);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse1.type);
+        assertEquals(0.0D, errorResponse1.balance, 0.0D);
+
+        EconomyResponse errorResponse2 = this.economy.depositPlayer(mockedOfflinePlayer2, "UnitTestWorld", 10.0D);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse2.type);
+        assertEquals(0.0D, errorResponse2.balance, 0.0D);
     }
 
     @Test
@@ -147,6 +205,10 @@ public class TestVault {
         assertEquals(EconomyResponse.ResponseType.SUCCESS, response.type);
 
         assertTrue(Common.getInstance().getAccountManager().exist(BANK_ACCOUNT, true));
+
+        // Test what happens if bank account already exists
+        EconomyResponse response2 = this.economy.createBank(BANK_ACCOUNT, accountOwner);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, response2.type);
     }
 
 
@@ -198,13 +260,41 @@ public class TestVault {
 
     @Test
     public void testBankWithdraw() {
-        // TODO
+        final String defaultCurrencyName = Common.getInstance().getCurrencyManager().getDefaultCurrency().getName();
+
+        // Create test bank account and set its balance
+        Account account = Common.getInstance().getAccountManager().getAccount(BANK_ACCOUNT, true);
+        account.set(100.0D, "UnitTestWorld", defaultCurrencyName, Cause.UNKNOWN, "Unittest");
+
+        EconomyResponse response = this.economy.bankWithdraw(BANK_ACCOUNT, 10.0D);
+        assertEquals(EconomyResponse.ResponseType.SUCCESS, response.type);
+        assertEquals(90.0D, response.balance, 0.0D);
+
+
+        // Test default (Error response) without creating an account
+        EconomyResponse errorResponse = this.economy.bankWithdraw("unknown", 10.0D);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse.type);
+        assertEquals(0.0D, errorResponse.balance, 0.0D);
     }
 
 
     @Test
     public void testBankDeposit() {
-        // TODO
+        final String defaultCurrencyName = Common.getInstance().getCurrencyManager().getDefaultCurrency().getName();
+
+        // Create test bank account and set its balance
+        Account account = Common.getInstance().getAccountManager().getAccount(BANK_ACCOUNT, true);
+        account.set(100.0D, "UnitTestWorld", defaultCurrencyName, Cause.UNKNOWN, "Unittest");
+
+        EconomyResponse response = this.economy.bankDeposit(BANK_ACCOUNT, 10.0D);
+        assertEquals(EconomyResponse.ResponseType.SUCCESS, response.type);
+        assertEquals(110.0D, response.balance, 0.0D);
+
+
+        // Test default (Error response) without creating an account
+        EconomyResponse errorResponse = this.economy.bankDeposit("unknown", 10.0D);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse.type);
+        assertEquals(0.0D, errorResponse.balance, 0.0D);
     }
 
 
@@ -222,6 +312,10 @@ public class TestVault {
         testAccount.getAccountACL().set(mockedOfflinePlayer.getName(), true, true, true, true, true);
         EconomyResponse successfullResponse = this.economy.isBankOwner(BANK_ACCOUNT, mockedOfflinePlayer);
         assertEquals(EconomyResponse.ResponseType.SUCCESS, successfullResponse.type);
+
+        // Test account does not exist
+        EconomyResponse errorResponse = this.economy.isBankOwner("unknownbankaccount", mockedOfflinePlayer);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse.type);
     }
 
 
@@ -239,17 +333,33 @@ public class TestVault {
         testAccount.getAccountACL().set(mockedOfflinePlayer.getName(), true, true, true, true, false);
         EconomyResponse successfullResponse = this.economy.isBankMember(BANK_ACCOUNT, mockedOfflinePlayer);
         assertEquals(EconomyResponse.ResponseType.SUCCESS, successfullResponse.type);
+
+        // Test account does not exist
+        EconomyResponse errorResponse = this.economy.isBankMember("unknownbankaccount", mockedOfflinePlayer);
+        assertEquals(EconomyResponse.ResponseType.FAILURE, errorResponse.type);
     }
 
 
     @Test
     public void testGetBanks() {
-        // TODO
+        // Create test account
+        Common.getInstance().getAccountManager().getAccount(BANK_ACCOUNT, true);
+        assertTrue(Common.getInstance().getAccountManager().exist(BANK_ACCOUNT, true));
+
+        assertEquals(1, this.economy.getBanks().size());
+        assertEquals(BANK_ACCOUNT, this.economy.getBanks().get(0));
     }
 
 
     @Test
     public void testCreatePlayerAccount() {
-        // TODO
+        final MockedOfflinePlayer accountOwner = new MockedOfflinePlayer(TEST_USER.getUuid(), TEST_USER.getName());
+        assertFalse(Common.getInstance().getAccountManager().exist(accountOwner.getName(), false));
+
+        assertTrue(this.economy.createPlayerAccount(accountOwner));
+        assertTrue(Common.getInstance().getAccountManager().exist(accountOwner.getName(), false));
+
+        // Test what happens if bank account already exists
+        assertFalse(this.economy.createPlayerAccount(accountOwner));
     }
 }
